c6b329cbb999d67596be75ef6520cac0e4fd5855
Corix5 <luisdavid202075@gmail.com>
2025-08-04 16:03:19 -0600

    Se agregó funcionalidad de botón de filtro resaltado y settimeout para renderizado

--- src/main.js
+++ src/main.js
@@ -12,21 +12,29 @@ let currentFilterStatus = "all";
 
 const onRemove = (id, currentFilterStatus) => {
   currentExtensions = currentExtensions.filter((ext) => ext.id !== id);
-  if(currentFilterStatus === "all") {
+  if (currentFilterStatus === "all") {
     renderCards(currentExtensions, currentFilterStatus);
-  } else if(currentFilterStatus === "active") {
+  } else if (currentFilterStatus === "active") {
     renderCards(filterByStatus("active"), currentFilterStatus);
-  } else{
+  } else {
     renderCards(filterByStatus("inactive"), currentFilterStatus);
   }
 };
 
-const renderCards = (extensionsToRender, currentFilterStatus) => {  
+const renderCards = (extensionsToRender, currentFilterStatus) => {
   container.innerHTML = "";
   extensionsToRender.forEach((extension) => {
-    const card = createExtensionCard(extension, onRemove, toggleCheckbox, currentFilterStatus);
+    const card = createExtensionCard(
+      extension,
+      onRemove,
+      toggleCheckbox,
+      currentFilterStatus
+    );
     container.appendChild(card);
   });
+
+  const noExtensionMessage = document.getElementById("no-extension-message");
+  noExtensionMessage.classList.toggle("active", extensionsToRender.length === 0);
 
   const savedTheme = localStorage.getItem("theme");
   const isLightTheme = savedTheme === "light";
@@ -40,39 +48,58 @@ const filterByStatus = (status) => {
   );
 };
 
-const toggleCheckbox = (id, status) => {  
+const toggleCheckbox = (id, status) => {
   const ext = currentExtensions.find((e) => e.id === id);
   if (ext) ext.isActive = !ext.isActive;
-  if (status === "active") {
-    renderCards(filterByStatus("active"), 'active');
-  } else if (status === "inactive") {
-    renderCards(filterByStatus("inactive"), 'inactive');
-  } else {
-    renderCards(currentExtensions, 'all');
-  }
+  setTimeout(() => {
+    if (status === "active") {
+      renderCards(filterByStatus("active"), "active");
+    } else if (status === "inactive") {
+      renderCards(filterByStatus("inactive"), "inactive");
+    } else {
+      renderCards(currentExtensions, "all");
+    }
+  }, 500);
 };
 
 const { extensionHeader, themeToggleBtn, themeIcon } = createHeader();
 
 headerController.appendChild(extensionHeader);
 
-filters.appendChild(
-  createExtensionFilter("All", () => {
-    currentFilterStatus = "all";
-    renderCards(filterByStatus("all"), currentFilterStatus);
-  })
-);
-filters.appendChild(
+const updateSelectedButton = () => {
+  allButton.classList.toggle("selected", currentFilterStatus === "all");
+  activeButton.classList.toggle("selected", currentFilterStatus === "active");
+  inactiveButton.classList.toggle(
+    "selected",
+    currentFilterStatus === "inactive"
+  );
+};
+
+const allButton = createExtensionFilter("All", () => {
+  currentFilterStatus = "all";
+  renderCards(filterByStatus("all"), currentFilterStatus);
+  updateSelectedButton();
+});
+
+const activeButton = filters.appendChild(
   createExtensionFilter("Active", () => {
     currentFilterStatus = "active";
     renderCards(filterByStatus("active"), currentFilterStatus);
-  })
-);
-filters.appendChild(
-  createExtensionFilter("Inactive", () => {
-    currentFilterStatus = "inactive";
-    renderCards(filterByStatus("inactive"), currentFilterStatus);
+    updateSelectedButton();
   })
 );
 
+const inactiveButton = filters.appendChild(
+  createExtensionFilter("Inactive", () => {
+    currentFilterStatus = "inactive";
+    renderCards(filterByStatus("inactive"), currentFilterStatus);
+    updateSelectedButton();
+  })
+);
+
+filters.appendChild(allButton);
+filters.appendChild(activeButton);
+filters.appendChild(inactiveButton);
+
 renderCards(currentExtensions, currentFilterStatus);
+updateSelectedButton();
